64b8b327121989273e42f9ae0efd2b0a42069b55
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-17 13:51:33 +0000

    Print line in stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - A function which will print line in the standard error
- * Return:1 if Success
- */
+ * Return: 1 if Success
+ **/
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
