81c2034ce6d0ac76786c097196f445cb4f5c1cdc
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-17 08:07:35 +0000

    Write a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+ *The main -- The essential part where the program will be writte
+ n
+ There is the return 0; -- to end the program in the main
+ */
 
-int main()
+int main(void)
 {
-	char prog[] ="Programming is like building a multilingual puzzle";
-	puts(prog);
+	puts("Programming is like building a multilingual puzzle\n");
+
 	}
 return 0;	
