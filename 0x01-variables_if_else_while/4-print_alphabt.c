f80bafde248a5bada7b17fd5151fd50e343f1269
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-18 10:08:55 +0000

    Omitting letters out of the alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The main function to print the program
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			if((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
