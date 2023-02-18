a2f1854af19102e0b6b5a6ba6d33d6e77b142ca2
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-18 09:23:47 +0000

    Listing upper & lower case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point for testing the codes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char e;
+
+	c = 'a';
+	e = 'A';
+
+	while
+		(c <= 'z')
+		{
+
+		putchar(c);
+		c++;
+	}
+		
+	while
+		(e <= 'Z')
+
+			{
+				putchar(c);
+				e++;
+			}
+		putchar('\n');
+
+	return (0);
+}
