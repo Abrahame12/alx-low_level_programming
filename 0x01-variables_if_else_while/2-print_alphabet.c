1a76763ebfe7067ea6ac10983f5488dec9aca258
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-18 08:27:40 +0000

    Listing alphabets from a - z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Help to execute print alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
