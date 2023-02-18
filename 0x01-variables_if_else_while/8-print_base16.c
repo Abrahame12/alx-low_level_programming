c9e31c634b0c86314a97d8301b1cf7120d4a22d7
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-18 12:20:49 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,22 @@
  */
 int main(void)
 {
-	int a;
+	char c;
 
-	for (a = 0; a < 10; a++)
-	{
-		printf("%x", a);
-	}
-	putchar(a);
-	a++;
+	int d;
+
+	c  = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
 	return (0);
 }
