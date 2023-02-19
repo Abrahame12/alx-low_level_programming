750bf29bc1aac74ebdbf5ba1b994afc1f2d1a2b2
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-19 07:42:49 -0800

    Combination of 2 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int num1, num2;
+
 	for (num1 = 0; num1 <= 8; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			putchar(num1 + '0');
 			putchar(num2 + '0');
-				if (num1 == 8 && num2 == 9)
-					continue;
-				putchar(44);
-				putchar(32);
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(44);
+			putchar(32);
 		}
 	}
 	putchar('\n');
