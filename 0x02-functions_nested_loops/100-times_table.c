71402a27754423ab7cce3010de56daf8983e8b43
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-23 11:39:24 +0000

    Print time tables of various inputs

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table of given inputs
+ *
+ * @n: The value of the times table to be printed
+ */
+void print_times_table(int n)
+{
+	int v, b, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (v = 0; v <= n; v++)
+		{
+			_putchar('0');
+
+			for (b = 0; b <= n; b++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				k = v * b;
+				if (k <= 99)
+					_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
