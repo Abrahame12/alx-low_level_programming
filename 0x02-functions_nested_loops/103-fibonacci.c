fa5ac90dcada8ae3cabe880942804d389af8b563
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-23 13:17:52 -0800

    fibonacci- determine the next value by adding the prevoius two values

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	unsigned long fb1, fb2, fbsum;
+	float total_sum;
+
+	while (1)
+	{
+		fbsum = fb1 + fb2;
+		if (fbsum > 4000000)
+			break;
+		if ((fbsum % 2) == 0)
+			total_num += fbsum;
+		fb1 = fb2;
+		fb2 = fbsum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
