80860e2d775f00b7acba7d398befe7ae15976de8
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-27 19:30:34 -0800

    Prints lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return lenght value
+ * @s : character to check
+ *
+ * Return: 0(Success)
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
