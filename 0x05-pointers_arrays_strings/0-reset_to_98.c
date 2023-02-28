9c908507113d1890aa23aef4b810c3ebddc0a4c5
Abraham Ezeh <ikezeh2021@gmail.com>
2023-02-27 19:09:45 -0800

    Pointer to take the int as parameter to print an assigned value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - prints the the pointer of ia parameter
+ * @n: holds the n values
+ *
+ * Return: 0(Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
