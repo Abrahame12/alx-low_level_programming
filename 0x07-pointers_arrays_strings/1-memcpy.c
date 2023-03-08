f722232c032518cd3648657ad8d1b8bab4232812
Abraham Ezeh <ikezeh2021@gmail.com>
2023-03-07 23:14:43 -0800

    Copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - writes copied memory area
+ * @src: source of the memory
+ * @dest: destination of the memory
+ * @n: number of bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
