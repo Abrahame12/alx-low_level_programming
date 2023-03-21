a27663e86b479f497af5159dfc2cda99eeb86d78
Abraham Ezeh <ikezeh2021@gmail.com>
2023-03-20 21:21:21 -0700

    Startups

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar -writes character c to stdout
+ * @c: prints characters
+ *
+ * Return: 1(Success)
+ * On error, -1 is returned and the error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
